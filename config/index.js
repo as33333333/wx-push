9011b5c141142215f2d8558b201c15c40ee24a41
as33333333 <130817439+as33333333@users.noreply.github.com>
2023-10-10 14:21:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -3,7 +3,9 @@ module.exports = {
   appsecret: "c0aea99bdc732131d4269fb8d52ebf76",
   template_id: "4trOn9kiMAT9yA_x4XNasVJO-bTkEmYLfvhEircrr9Y",
   touser: "oxoqv6eBoR0exF0tSHg0kUt3RboA",
-
+ key: "201c93c2e2b34bdeecdbf9f5265eb84e",
+  city: "烟台",
+  first_date: "2022-11-10",
 
 
 };
