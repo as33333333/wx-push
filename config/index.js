502777521abec337e6537baf41cbc48ae8d39cd3
as33333333 <130817439+as33333333@users.noreply.github.com>
2023-10-10 14:13:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxd5f1a77fd9a3ff17",
+  appsecret: "c0aea99bdc732131d4269fb8d52ebf76",
+  template_id: "4trOn9kiMAT9yA_x4XNasVJO-bTkEmYLfvhEircrr9Y",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
+  city: "烟台",
   first_date: "2022-11-10",
 };
